71b06cac29ff6ddfd42b8b68cff0a47462aa99f0
Juan Miguel Rúa <juanmiguel.rua.dev@gmail.com>
2026-06-09 16:54:22 +0200

    treesitter configuration

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -1,6 +1,55 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter').setup {
+        install_dir = vim.fn.stdpath 'data' .. '/site',
+      }
+
+      -- Install parsers for the desired languages
+      require('nvim-treesitter').install {
+        'bash',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'vim',
+        'vimdoc',
+        'go',
+        'gomod',
+        'gosum',
+        'terraform',
+        'dockerfile',
+        'gitignore',
+        'json',
+        'rust',
+      }
+
+      -- Enable treesitter highlighting for all supported filetypes
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'sh',
+          'bash',
+          'lua',
+          'markdown',
+          'vim',
+          'help',
+          'go',
+          'gomod',
+          'gosum',
+          'terraform',
+          'dockerfile',
+          'gitignore',
+          'json',
+          'rust',
+        },
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
+    end,
   },
 }
